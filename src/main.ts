cf01447124f8a074ad1966e92287b1694ddb3c71
Shaun Grady <grady@shaungrady.com>
2023-04-18 22:58:57 -0700

    Fix package name, `main` exports

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 export { default as EnumSet } from './classes/EnumSet';
 export { default as EnumMap } from './classes/EnumMap';
 
+export { default as enumToSet } from './functions/enumToSet';
+export { default as isEnumMember } from './functions/isEnumMember';
 export { default as isValidEnumMember } from './functions/isValidEnumMember';
-export { default as enumToArray } from './functions/enumToSet';
