7342fe329c99c2cb5ea4f2aea2c8d05cfdccb0b0
Shaun Grady <grady@shaungrady.com>
2024-07-30 17:40:57 -0700

    Fix `TS2742` errors related to EnumMap

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ export { default as enumToSet } from './functions/enumToSet.js';
 export { default as isEnumMember } from './functions/isEnumMember.js';
 export { default as isValidEnumMember } from './functions/isValidEnumMember.js';
 
+// Fixes downstream `TS2742` errors:
+//   The inferred type of "X" cannot be named without a reference to "Y".
+//
+// This is related to package A using `enum-utils` and exporting an `EnumSet`,
+// then package B importing that `EnumSet` and calling `toEnumMap()`.
+export type { Opaque } from 'type-fest';
+
 export type { EnumSetMembers } from './types/EnumSetMembers.js';
 export type { EnumMapMembers } from './types/EnumMapMembers.js';
 export type { EnumMapValues } from './types/EnumMapValues.js';
